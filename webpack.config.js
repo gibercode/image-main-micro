4a20f5163e2394d5802d0dcd44ca7bf085f039df
gibercode <gibersonlara@Gibersons-MacBook-Pro.local>
2024-09-05 19:36:30 -0400

    entry point changed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: "./src/bootstrap.tsx",
   output: {
     filename: "[name].[contenthash].js",
     publicPath: "/",
