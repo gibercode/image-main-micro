d758017da70412cc76746bd7be3ac42b21524b46
gibercode <gibersonlara@gibersons-mbp.lan>
2024-09-06 14:12:26 -0400

    loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { MainLayout } from "./components";
+import { Spin } from "antd";
 import "./styles.css";
+
 const SearchBar = React.lazy(() => import("searchBar/SearchBar"));
 const List = React.lazy(() => import("list/List"));
 
 const App = () => {
   return (
     <MainLayout>
-      <React.Suspense fallback={<div>Loading...</div>}>
+      <React.Suspense
+        fallback={
+          <div className="spinnerContainer">
+            <Spin size="large" />
+          </div>
+        }
+      >
         <SearchBar />
         <List />
       </React.Suspense>
